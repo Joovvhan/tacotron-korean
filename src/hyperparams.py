43365b6de8fbd79431bcf970cf0162af5ca0c50e
Your Name <joovvhan.sonq@gmail.com>
2019-02-13 15:08:50 +0900

    Start imitating

---
+++ src/hyperparams.py
@@ -0,0 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+
+class Hyperparams:
+
+	vocab = " ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣㄳㄵㄶㄺㄻㄼㄽㄾㄿㅀㅄ.,?!"
+	data_dir = 'D:/korean-single-speaker-speech-dataset/kss'
+	transcript_pos = 'D:/korean-single-speaker-speech-dataset/kss/transcript.txt'
+	
