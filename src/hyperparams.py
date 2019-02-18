03bc13fa78430b69119a997875a525639ba6cc47
Your Name <joovvhan.sonq@gmail.com>
2019-02-18 10:45:23 +0900

    Added prenet and convolutional banks

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -12,10 +12,13 @@ class Hyperparams:
 	n_mels = 80
 	db_limit = -60
 	offset = 2
+	dropout_rate = 0.5
 	
 	eps = 1e-5
 	max_db = 100
 	embed_size = 256
+	num_prenet_node_1 = 256
+	num_prenet_node_2 = 128
 	num_k_filter = embed_size//2
 	K = 16
 	
