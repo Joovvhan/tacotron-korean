fc18638a2d242fdbc1feab04fd35dd8f0723f70d
Your Name <joovvhan.sonq@gmail.com>
2019-02-20 09:09:43 +0900

    Implemented every graph within an iPython Notebook file.

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -14,11 +14,21 @@ class Hyperparams:
 	offset = 2
 	dropout_rate = 0.5
 	
+	r = 5
+	
 	eps = 1e-5
 	max_db = 100
 	embed_size = 256
 	num_prenet_node_1 = 256
 	num_prenet_node_2 = 128
 	num_k_filter = embed_size//2
+	num_conv1d_proj_filter = embed_size//2
+	size_conv1d_proj_filter = 3
+	num_highwaynet_blocks = 4
+	num_highwaynet_units = embed_size//2
+	num_gru_units = embed_size//2
+	
+	num_attention_units = embed_size//2
+	
 	K = 16
 	
